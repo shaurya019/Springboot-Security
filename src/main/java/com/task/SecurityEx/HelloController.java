9ca96bbef081f22b0b50df9bb54f503dcec9b6b2
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-23 22:06:45 +0530

    Get session id

--- src/main/java/com/task/SecurityEx/HelloController.java
+++ src/main/java/com/task/SecurityEx/HelloController.java
@@ -1,4 +1,17 @@
 package com.task.SecurityEx;
 
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "HEY" + request.getSession().getId();
+    }
+
 }
