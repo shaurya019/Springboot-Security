776d91c817bb15d39760983f25c039eb5c03754d
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-23 22:02:45 +0530

    first commit

---
+++ src/main/java/com/task/SecurityEx/StudentController.java
@@ -0,0 +1,4 @@
+package com.task.SecurityEx;
+
+public class StudentController {
+}
---
+++ src/test/java/com/task/SecurityEx/SecurityExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.SecurityEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
