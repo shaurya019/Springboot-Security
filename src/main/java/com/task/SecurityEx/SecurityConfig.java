df09d71e24afffcf7afb8684547aefd8897f5f1d
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-12-23 22:22:09 +0530

    Security config

--- src/main/java/com/task/SecurityEx/SecurityConfig.java
+++ src/main/java/com/task/SecurityEx/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.task.SecurityEx;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http.csrf(customizer -> customizer.disable())
+                   .authorizeHttpRequests(request ->request.anyRequest().authenticated())
+                   .httpBasic(Customizer.withDefaults())
+                   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
 }
